da8286b407b675f23106097bcc1b72414579653c
Baker6Romeo <43591783+Baker6Romeo@users.noreply.github.com>
2022-03-22 09:25:26 -0700

    Complete basic app setup, shopping list and ingredient add

--- recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.modle';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [new Ingredient('Whiskey', 2), new Ingredient('Steak', 1)];
 
   constructor() { }
 
